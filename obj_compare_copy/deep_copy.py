bce53262d1b4b6fdc235a4c578b011411093b34e
liusha <shaojun.liu@autodesk.com>
2020-02-27 01:05:46 +0800

    update

--- obj_compare_copy/deep_copy.py
+++ obj_compare_copy/deep_copy.py
@@ -28,7 +28,7 @@ print('y returns {}'.format(y))
 
 # 思考题
 # 当我们用等于操作符'=='进行比较时，输出会是什么呢？
-print('x == y returns {}'.format(x == y))
+# print('x == y returns {}'.format(x == y))
 # 答案
 # 程序会报错：'RecursionError: maximum recursion depth exceeded in comparison'。
 # 因为x是一个无限嵌套的列表，y深度拷贝x也是一个无限嵌套的列表，理论上x==y应该返回True，
@@ -38,3 +38,47 @@ print('x == y returns {}'.format(x == y))
 # 一开始x只有一个元素，[1]，调用append方法时，长度会加1，并准备将新元素写入，x变成[1, [新元素]]，
 # 但这个新元素是x自身，x现在已经是[1, [新元素]]，于是你把x放到第一个新元素的位置，
 # x就从[1, [新元素] ] 就变成了[1, [1, [新元素]]]，此时新元素指向的依然是x，所以总会有个新元素。
+
+
+import copy
+# a = 1  # 不可变数据类型
+# copy_a = copy.deepcopy(a)
+# print(id(a)==id(copy_a))  
+
+# a = [1,2]  # 可变数据类型
+# copy_a = copy.deepcopy(a)
+# print(id(a)==id(copy_a))  
+# print(id(a[0])==id(copy_a[0]))
+
+# a = ([1,2],(3,4))  # 不可变数据类型
+# copy_a = copy.deepcopy(a)
+# print(id(a)==id(copy_a))  
+# print(id(a[0])==id(copy_a[0]))
+# print(id(a[1])==id(copy_a[1]))
+
+# a = ((1,2),(3,4))  # 不可变数据类型
+# copy_a = copy.deepcopy(a)
+# print(id(a)==id(copy_a))  
+# print(id(a[0])==id(copy_a[0]))
+# print(id(a[1])==id(copy_a[1]))
+
+# a = [(1,2),[3,4]]  # 可变数据类型
+# copy_a = copy.deepcopy(a)
+# print(id(a)==id(copy_a))  
+# print(id(a[0])==id(copy_a[0]))  
+# print(id(a[1])==id(copy_a[1]))  
+
+a = [(1,2),(3,4)]  # 可变数据类型
+copy_a = copy.deepcopy(a)
+print(id(a)==id(copy_a))  
+print(id(a[0])==id(copy_a[0]))
+print(id(a[1])==id(copy_a[1]))
+
+
+# 深拷贝，无论外层可变or不可变，只要内层有可变，则对象与拷贝对象地址不同
+# the same way, 无论外层可变or不可变，只要内层是不可变，则内层对象地址相同
+# 无论外层可变or不可变，只要内层是可变，则内层对象地址不相同
+
+# 可变对象是指，一个对象在不改变其所指向的地址的前提下，可以修改其所指向的地址中的值；
+
+# 不可变对象是指，一个对象所指向的地址上值是不能修改的，如果你修改了这个对象的值，那么它指向的地址就改变了，相当于你把这个对象指向的值复制出来一份，然后做了修改后存到另一个地址上了，但是可变对象就不会做这样的动作，而是直接在对象所指的地址上把值给改变了，而这个对象依然指向这个地址。
